601932ddae373731428e59438a5d8a15cd0b4cbc
Asnebula <Asenbula@sina.com>
2018-08-08 23:51:21 +0800

    fix bug

--- cn_stock_holidays/zipline/__init__.py
+++
@@ -1,8 +0,0 @@
-from cn_stock_holidays.zipline.exchange_calendar_hkex import HKExchangeCalendar
-from cn_stock_holidays.zipline.exchange_calendar_shsz import SHSZExchangeCalendar
-
-
-__all__ = [
-    'HKExchangeCalendar',
-    'SHSZExchangeCalendar',
-]
