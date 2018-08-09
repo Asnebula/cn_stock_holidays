5057b47db6a616547f7c9f97d8d8fa55afb71fc0
Asnebula <Asenbula@sina.com>
2018-08-09 11:17:55 +0800

    update setup

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
 
     package_data={
         # If any package contains *.txt or *.rst files, include them:
-        'cnhd': ['*.txt'],
+        'files': ['*.txt'],
     },
     entry_points={
         'console_scripts': [
