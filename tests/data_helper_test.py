ef81337cb88fed87616b47ac3fea8d5130c773e3
Asnebula <Asenbula@sina.com>
2018-08-09 19:33:38 +0800

    update test code

--- tests/data_helper_test.py
+++ tests/data_helper_test.py
@@ -1,21 +1,20 @@
-#coding: utf-8
+# coding: utf-8
 
 import unittest
+from cnhd.common import *
+from cnhd import CalendarTool as ct
 
-from cnhd.data_hk import *
+cthk = ct('HK')
 
 
-class TestHkData(unittest.TestCase):
-
-
+class TestCalendarTool(unittest.TestCase):
 
     def test_get_cached(self):
         """
         get cached related
         """
-        data = get_cached()
-        data2 = get_cached()
-
+        data = cthk.get_cached()
+        data2 = cthk.get_cached()
 
         self.assertEqual(str(data), str(data2), "get_cached 2 times give some results")
 
@@ -25,36 +24,34 @@ class TestHkData(unittest.TestCase):
 
         self.assertTrue(datetime.date(2000, 12, 25) in data, "get datetime.date(2000, 12, 25) in cached data")
 
-
     def test_trading_days_between(self):
-        data = list(trading_days_between(int_to_date(20170125), int_to_date(20170131)))
+        data = list(cthk.trading_days_between(int_to_date(20170125), int_to_date(20170131)))
 
         self.assertEqual(len(data), 3)
         self.assertTrue(int_to_date(20170125) in data)
         self.assertTrue(int_to_date(20170126) in data)
         self.assertTrue(int_to_date(20170127) in data)
 
-
     def test_is_trading_day(self):
-        self.assertIsNotNone(is_trading_day(datetime.date.today()))
+        self.assertIsNotNone(cthk.is_trading_day(datetime.date.today()))
 
     def test_next_trading_day(self):
-        data = next_trading_day(datetime.date.today())
+        data = cthk.next_trading_day(datetime.date.today())
         self.assertGreater(data, datetime.date.today())
 
     def test_previous_trading_day(self):
-        data = previous_trading_day(datetime.date.today())
+        data = cthk.previous_trading_day(datetime.date.today())
         self.assertLess(data, datetime.date.today())
 
     def test_cache_clear(self):
-        data = get_cached()
-        get_cached.cache_clear()
-        data2 = get_cached()
+        data = cthk.get_cached()
+        cthk.get_cached.cache_clear()
+        data2 = cthk.get_cached()
         self.assertEqual(str(data), str(data2), "get_cached 2 times give some results")
 
     def test_loop_100000(self):
         trade_days = datetime.date.today()
         for i in range(100000):
-            trade_days = previous_trading_day(trade_days)
+            trade_days = cthk.previous_trading_day(trade_days)
 
         self.assertIsInstance(trade_days, datetime.date)
